f3016e4629b6ff549f1f398fe834a98ae1128ebf
AmirMohammad Deilami <a.m.deilami@gmail.com>
2025-09-16 00:33:20 -0700

    added vecotr-scalar multiplication kernel, might add random inputs to it later

---
+++ day-002/README.md
@@ -0,0 +1,3 @@
+## Summary:
+
+scaling up or down all elements of a vector all at once.
---
+++ day-002/day-002-vectorCoefficient.cu
@@ -0,0 +1,65 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void vectorCoefficientKernel(float *A_d, float *R_d, float coefficient, int n)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n)
+    {
+        R_d[i] = A_d[i] * coefficient;
+    }
+}
+
+__host__ void vectorCoefficient(float *A_h, float *R_h, float coefficient, int n)
+{
+    // allocating memory on device
+    int size = n * sizeof(float);
+
+    float *A_d, *R_d;
+    cudaError_t err = cudaMalloc((void **)&A_d, size);
+    if (err != cudaSuccess)
+    {
+        printf("There was something wrong with cuda memory allocation: %s", cudaGetErrorString(err));
+    }
+    cudaMalloc((void **)&R_d, size);
+
+    // transfering data from host
+    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+
+    // calling the kernel...
+    vectorCoefficientKernel<<<ceil(n / 256.0), 256>>>(A_d, R_d, coefficient, n);
+
+    // transfering result to host
+    cudaMemcpy(R_h, R_d, size, cudaMemcpyDeviceToHost);
+
+    // deallocating what we don't need anymore
+    cudaFree(A_d);
+    cudaFree(R_d);
+}
+
+int main(int argc, char *argv[])
+{
+    int n = atoi(argv[1]);
+    float *A_h = (float *)malloc(n * sizeof(float));
+    float *R_h = (float *)malloc(n * sizeof(float));
+
+    float coefficient = 3;
+
+    for (int i = 0; i < n; ++i)
+    {
+        A_h[i] = 2;
+        R_h[i] = 0;
+    }
+
+    vectorCoefficient(A_h, R_h, coefficient, n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%f ,", R_h[i]);
+    }
+
+    free(A_h);
+    free(R_h);
+
+    return 0;
+}
